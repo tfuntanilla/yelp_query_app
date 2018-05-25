17441564cd4d0b4de3d841f786728b15f382c986
trishafuntanilla <trishafuntanilla@gmail.com>
2018-05-24 21:42:45 -0700

    started gui design

--- src/hw3/HW3.java
+++ src/hw3/HW3.java
@@ -1,10 +1,123 @@
 package hw3;
 
-public class HW3 {
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HW3 extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+    private JComboBox comboBox5;
+    private JButton executeButton;
+
+    public HW3() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
 
     public static void main(String[] args) {
 
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Exception while loading oracle jdbc driver: " + e.getMessage());
+            System.out.println("Terminated program.");
+            System.exit(-1);
+        }
+
+        List<String> categories = new ArrayList<String>();
+        Connection connection = null;
+        try {
+
+            connection = DriverManager
+                    .getConnection(Constants.ORACLE_URL, Constants.USERNAME, Constants.PASSWORD);
+
+            categories = queryAllCategories(connection);
+
+        } catch (SQLException e) {
+            System.out.println("Exception while establishing connection: " + e.getMessage());
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("Exception while closing connection: " + e.getMessage());
+                System.exit(-1);
+            }
+        }
+
+        HW3 dialog = new HW3();
+        dialog.pack();
+        dialog.setVisible(true);
         System.exit(0);
     }
 
+    private static List<String> queryAllCategories(Connection conn) {
+
+        List<String> categories = new ArrayList<String>();
+
+        String sql = "SELECT C.CATEGORY FROM CATEGORY C";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                categories.add(rs.getString("CATEGORY"));
+            }
+            rs.close();
+        } catch(SQLException e) {
+            System.out.println("Exception while querying for categories: " + e.getMessage());
+        }
+
+        return categories;
+
+    }
 }
